9a628f99c5fb9203e603dc8f826a223763cb0bd8
serberoos <jae99c@gmail.com>
2022-05-17 17:44:56 +0900

    feat : implement logout controller

--- main/src/main/java/cookalone/main/controller/UserController.java
+++ main/src/main/java/cookalone/main/controller/UserController.java
@@ -1,12 +1,13 @@
 package cookalone.main.controller;
 
-import cookalone.main.domain.Address;
-import cookalone.main.domain.User;
 import cookalone.main.domain.dto.account.TermsDto;
 import cookalone.main.domain.dto.account.UserDto;
 import cookalone.main.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 /**
@@ -78,6 +81,20 @@ public class UserController {
         model.addAttribute("exception", exception);
 
         return "login_form";
+    }
+
+    /**
+     *  GET : 컨트롤러에서 매핑을 받고, 로그아웃 기능으로 우회한다.
+     */
+    /* 로그아웃 */
+    @GetMapping("/auth/logout-proc")
+    public String logoutProc(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
 
