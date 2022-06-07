525fa73825af391b722f21ad622bf5b4e094b880
JaeGeun.Song <jae99c@gmail.com>
2022-06-07 21:47:33 +0900

    build : add docker-compose

--- main/scripts/deploy.sh
+++ main/scripts/deploy.sh
@@ -10,7 +10,8 @@ sudo docker rmi jae99c/spring-cookalone:1.0
 sudo docker pull jae99c/spring-cookalone:1.0
 
 # 도커 run
-docker run -d -p 8080:8080 --name spring-cookalone jae99c/spring-cookalone:1.0
+docker-compose up
+#docker run -d -p 8080:8080 --name spring-cookalone jae99c/spring-cookalone:1.0
 
 # 사용하지 않는 불 필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않음.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
